37f0895a8dd63e05ef4f3c00633a682e805fa529
michaski <michaski86@gmail.com>
2023-03-19 23:59:31 +0100

    Added ErrorHandlingMiddleware

---
+++ PongServer/PongServer.Api/Installers/MiddlewareInstaller.cs
@@ -0,0 +1,12 @@
+﻿using PongServer.Api.Middleware;
+
+namespace PongServer.Api.Installers
+{
+    public class MiddlewareInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddScoped<ErrorHandlingMiddleware>();
+        }
+    }
+}
--- PongServer/PongServer.Api/Program.cs
+++ PongServer/PongServer.Api/Program.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using NLog.Web;
 using PongServer.Api.Installers;
+using PongServer.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 
