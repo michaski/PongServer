3e8c182210017fca7baff6ef78e94ff09fee6f70
michaski <michaski86@gmail.com>
2023-03-26 21:23:23 +0200

    Added email config

---
+++ PongServer/PongServer.Api/Installers/FluentEmailInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Mail;
+
+namespace PongServer.Api.Installers
+{
+    public class FluentEmailInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration Configuration)
+        {
+            var emailConfig = Configuration.GetSection("EmailConfig");
+            var smtpConfig = emailConfig.GetSection("SmtpSender");
+            services
+                .AddFluentEmail(
+                    emailConfig["Email"],
+                    emailConfig["Name"])
+                .AddSmtpSender(
+                    smtpConfig["Host"],
+                    int.Parse(smtpConfig["Port"]),
+                    smtpConfig["Username"],
+                    smtpConfig["Password"]);
+        }
+    }
+}
