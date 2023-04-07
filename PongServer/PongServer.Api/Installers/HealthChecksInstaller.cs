2e3ecc48ef21245f77880296f63930645f5fc4ac
michaski <michaski86@gmail.com>
2023-04-07 22:37:48 +0200

    Added health checks with DB status check

---
+++ PongServer/PongServer.Api/Installers/HealthChecksInstaller.cs
@@ -0,0 +1,13 @@
+﻿using PongServer.Infrastructure.Data;
+
+namespace PongServer.Api.Installers
+{
+    public class HealthChecksInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddHealthChecks()
+                .AddDbContextCheck<PongDataContext>();
+        }
+    }
+}
--- PongServer/PongServer.Api/Program.cs
+++ PongServer/PongServer.Api/Program.cs
@@ -1,8 +1,12 @@
 using System.Reflection;
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using NLog.Web;
 using PongServer.Api.Installers;
 using PongServer.Api.Middleware;
+using PongServer.Application.Dtos.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +20,11 @@ builder.Host.UseNLog();
 var app = builder.Build();
 
 var apiVersionDescriptorProvier = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = WriteHealthCheckResponseAsync
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -41,3 +50,20 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+async Task WriteHealthCheckResponseAsync(HttpContext context, HealthReport report)
+{
+    context.Response.ContentType = "application/json";
+    var response = new HealthCheckResponse
+    {
+        Status = report.Status.ToString(),
+        Checks = report.Entries.Select(entry => new HealthCheck
+        {
+            Component = entry.Key,
+            Status = entry.Value.Status.ToString(),
+            Description = entry.Value.Description
+        }),
+        Duration = report.TotalDuration
+    };
+    await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+}
