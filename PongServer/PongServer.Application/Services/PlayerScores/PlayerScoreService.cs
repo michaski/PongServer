feb1435694817da211dc63a4842cce3302dae845
michaski <michaski86@gmail.com>
2023-04-10 19:58:37 +0200

    Added rank scoring

--- PongServer/PongServer.Application/Services/PlayerScores/PlayerScoreService.cs
+++ PongServer/PongServer.Application/Services/PlayerScores/PlayerScoreService.cs
@@ -8,6 +8,7 @@ using PongServer.Application.Dtos.V1.Pagination;
 using PongServer.Application.Dtos.V1.PlayerScores;
 using PongServer.Application.Services.UserContext;
 using PongServer.Domain.Enums;
+using PongServer.Domain.HelperMethods;
 using PongServer.Domain.Interfaces;
 using PongServer.Domain.Utils;
 
@@ -20,8 +21,8 @@ namespace PongServer.Application.Services.PlayerScores
         private readonly IUserContextService _userContextService;
 
         public PlayerScoreService(
-            IPlayerScoreRepository playerScoreRepository, 
-            UserManager<IdentityUser> userManager, 
+            IPlayerScoreRepository playerScoreRepository,
+            UserManager<IdentityUser> userManager,
             IUserContextService userContextService)
         {
             _playerScoreRepository = playerScoreRepository;
@@ -49,12 +50,18 @@ namespace PongServer.Application.Services.PlayerScores
             }
 
             var playerScore = await _playerScoreRepository.GetPlayersScoreAsync(player);
+            var opponentScore = await _playerScoreRepository.GetPlayersScoreAsync(opponent);
+
             playerScore.GamesPlayed += 1;
             playerScore.GamesWon += (int)scoreDto.MatchResult;
+            playerScore.RankingScore = EloScoreCalculator.CalculateScore(scoreDto.MatchResult, 
+                playerScore.RankingScore, opponentScore.RankingScore);
 
-            var opponentScore = await _playerScoreRepository.GetPlayersScoreAsync(opponent);
+            var oppositeMatchResult = scoreDto.MatchResult == GameResult.Won ? GameResult.Lost : GameResult.Won;
             opponentScore.GamesPlayed += 1;
-            opponentScore.GamesWon += scoreDto.MatchResult == GameResult.Won ? 0 : 1;
+            opponentScore.GamesWon += (int)oppositeMatchResult;
+            opponentScore.RankingScore = EloScoreCalculator.CalculateScore(oppositeMatchResult,
+                opponentScore.RankingScore, playerScore.RankingScore);
 
             await _playerScoreRepository.UpdateScoreAfterGameAsync(playerScore, opponentScore);
             return true;
---
+++ PongServer/PongServer.Domain/HelperMethods/EloScoreCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PongServer.Domain.Enums;
+
+namespace PongServer.Domain.HelperMethods
+{
+    public static class EloScoreCalculator
+    {
+        public static int CalculateScore(GameResult gameResult, int playerScore, int opponentScore)
+        {
+            var k = 32;
+            if (playerScore > 2400)
+            {
+                k = 16;
+            }
+
+            return (int)Math.Round(playerScore + k * ((int)gameResult - CalculateWinChance(playerScore, opponentScore)), 0);
+        }
+
+        public static double CalculateWinChance(int playerScore, int opponentScore)
+        {
+            return 1.0 / (1.0 + Math.Pow(10, (playerScore - opponentScore) / 400.0));
+        }
+    }
+}
